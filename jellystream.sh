55fc3e796e3cc5afe6edff115ae7426fa3f58c94
Jiab77 <jiab77@pm.me>
2024-08-12 19:15:46 +0200

    ✨ New release 0.2.2  - Moved common methods to external file  - Minor improvements

--- jellystream.sh
+++ jellystream.sh
@@ -6,6 +6,7 @@
 #
 # Todo:
 #  - Add 'mpd' support
+#  - Add 'vlc' support
 #  - Add 'gst123' support
 #  - Add 'ffplay' support
 #  - Add 'wget' support
@@ -15,24 +16,14 @@
 #  - Add 'mpv' support
 #  - Add 'mpg123' support
 #
-# Version 0.2.0
+# Version 0.2.2
 
 # Options
-set +o xtrace
-
-# Colors
-NC="\033[0m"
-NL="\n"
-BLUE="\033[1;34m"
-YELLOW="\033[1;33m"
-GREEN="\033[1;32m"
-RED="\033[1;31m"
-WHITE="\033[1;37m"
-PURPLE="\033[1;35m"
+[[ -r $HOME/.debug ]] && set -o xtrace || set +o xtrace
 
 # Config
 DEBUG_MODE=false
-CLEAR_SCREEN=true
+CLEAR_SCREEN=false
 SHOW_HEADER=true
 SERVER_ADDR="YOUR-SERVER-ADDRESS-WITH-PORT-IF-ANY"
 API_KEY="YOUR-API-KEY"
@@ -41,71 +32,43 @@ AUDIO_BITRATE=192000
 SAMPLE_RATE=48000
 BUFFER_SIZE=0
 MPV_GUI_ENABLED=false
+MPV_MIN_VER="0.34.0"
+CREATE_PLS_FILE=false
+LIMIT=20
 
 # Internals
-BIN_CURL=$(which curl 2>/dev/null)
-BIN_JQ=$(which jq 2>/dev/null)
-BIN_MPG123=$(which mpg123 2>/dev/null)
-BIN_MPV=$(which mpv 2>/dev/null)
-BIN_PLAYCTL=$(which playerctl 2>/dev/null)
-BIN_SED=$(which sed 2>/dev/null)
-SCRIPT_DIR="$(dirname "$0")"
-SCRIPT_FILE="$(basename "$0")"
-SCRIPT_NAME="${SCRIPT_FILE//.sh/}"
-CONFIG_FILE="${SCRIPT_NAME}.conf"
-CREATE_PLS_FILE=false
+BIN_CURL=$(command -v curl 2>/dev/null)
+BIN_JQ=$(command -v jq 2>/dev/null)
+BIN_MPG123=$(command -v mpg123 2>/dev/null)
+BIN_MPV=$(command -v mpv 2>/dev/null)
+BIN_VLC=$(command -v vlc 2>/dev/null)
+BIN_PLAYCTL=$(command -v playerctl 2>/dev/null)
+BIN_SED=$(command -v sed 2>/dev/null)
+
+# Common
+if [[ -r "$(dirname "$0")/common/bash_funcs" ]]; then
+  source "$(dirname "$0")/common/bash_funcs" || ( echo -e "\nUnable to load '[script_dir]/common/bash_funcs' file.\n" && exit 255 )
+fi
 
 # Functions
-function die() {
-    echo -e "${NL}${RED}Error: ${YELLOW}${1}${NC}${NL}" >&2
-    exit 255
-}
-function log() {
-    echo -e "$1" >&2
-}
-function get_version() {
-    grep -i 'version' "$0" | awk '{ print $3 }' | head -n1
-}
-function set_console_title() {
-    echo -ne "\033]0;${1}\007"
-}
 function print_header() {
     [[ $CLEAR_SCREEN == true ]] && reset
-    [[ $SHOW_HEADER == true ]] && log "${NL}${BLUE}Simple Cloud CLI Music Player based on ${PURPLE}Jellyfin${BLUE} - ${GREEN}v$(get_version)${NC}${NL}"
+    [[ $SHOW_HEADER == true ]] && log_err "${NL}${BLUE}Simple Cloud CLI Music Player based on ${PURPLE}Jellyfin${BLUE} - ${GREEN}v$(get_version)${NC}${NL}"
 }
 function print_usage() {
-    log "${NL}Usage: $SCRIPT_FILE [generate|status|stop|play|pause|prev|next]${NL}"
-    exit 1
+    log "${NL}Usage: $SCRIPT_FILE [flags] [generate|status|stop|play|pause|prev|next]"
+    log "${NL}Flags:"
+    log "  -h | --help\tPrint this message and exit"
+    log "  -l | --limit <value>\tLimit created playlist to given value (default: $LIMIT)"
+    log
+    exit
 }
-function load_xdg_defs() {
-    if [[ -r ~/.config/user-dirs.dirs ]]; then
-        # shellcheck source=/dev/null
-        source ~/.config/user-dirs.dirs
-    fi
-    [[ -z $XDG_CONFIG_HOME ]] && XDG_CONFIG_HOME="$HOME/.config"
-}
-function load_config_file() {
-    if [[ -r "$XDG_CONFIG_HOME/$CONFIG_FILE" ]]; then
-        # shellcheck source=/dev/null
-        source "$XDG_CONFIG_HOME/$CONFIG_FILE"
-    elif [[ -r "$XDG_CONFIG_HOME/$SCRIPT_NAME/$CONFIG_FILE" ]]; then
-        # shellcheck source=/dev/null
-        source "$XDG_CONFIG_HOME/$SCRIPT_NAME/$CONFIG_FILE"
-    elif [[ -r "$SCRIPT_DIR/$CONFIG_FILE" ]]; then
-        # shellcheck source=/dev/null
-        source "$SCRIPT_DIR/$CONFIG_FILE"
-    fi
-}
-function get_mpv_mpris_lib() {
-    local MPV_MPRIS_PATH
-    if [[ -r "$XDG_CONFIG_HOME/mpv/scripts/mpris.so" ]]; then
-        MPV_MPRIS_PATH="$XDG_CONFIG_HOME/mpv/scripts/mpris.so"
-    elif [[ -r /etc/mpv/scripts/mpris.so ]]; then
-        MPV_MPRIS_PATH=/etc/mpv/scripts/mpris.so
-    else
-        MPV_MPRIS_PATH=
-    fi
-    echo -n "$MPV_MPRIS_PATH"
+function bootstrap() {
+  set_script_dir
+  set_script_file
+  set_config_file
+  load_xdg_defs
+  load_config_file
 }
 function jellyfin_connect() {
     # Connect to Jellyfin server
@@ -113,18 +76,18 @@ function jellyfin_connect() {
     USER_ID=$(curl -sSL "${SERVER_ADDR}/Users?api_key=${API_KEY}" 2>/dev/null | jq -r '(.[] | select(.Name == "'${LOGIN_AS}'") | .Id)')
     CONNECTED_AS=$(curl -sSL "${SERVER_ADDR}/Users/${USER_ID}?api_key=${API_KEY}" 2>/dev/null | jq -r .Name)
     if [[ -n $CONNECTED_AS ]]; then
-        log " ${GREEN}connected${NC}${NL}"
+        log_err " ${GREEN}connected${NC}${NL}"
     else
-        log " ${RED}failed${NC}${NL}"
+        log_err " ${RED}failed${NC}${NL}"
         exit 1
     fi
     if [[ $DEBUG_MODE == true ]]; then
-        log "${PURPLE}[DEBUG]${WHITE} Connected as: ${YELLOW}${CONNECTED_AS}${NC}${NL}"
+        log_err "${PURPLE}[DEBUG]${WHITE} Connected as: ${YELLOW}${CONNECTED_AS}${NC}${NL}"
     fi
 }
 function get_user_info() {
     # Show connected user info
-    log "${PURPLE}[DEBUG]${WHITE} Displaying gathered user info:${NC}${NL}"
+    log_err "${PURPLE}[DEBUG]${WHITE} Displaying gathered user info:${NC}${NL}"
     curl -sSL "${SERVER_ADDR}/Users/${USER_ID}?api_key=${API_KEY}" | jq . >&2
     echo >&2
 }
@@ -133,33 +96,33 @@ function get_music_genres() {
     echo -ne "${WHITE}Gathering music genres...${NC}" >&2
     mapfile -t MUSIC_GENRES < <(curl -sSL "${SERVER_ADDR}/MusicGenres?api_key=${API_KEY}" 2>/dev/null | jq -r '.Items[] | .Name' | sed -e 's/ /_/gi')
     if [[ ${#MUSIC_GENRES[*]} -gt 0 ]]; then
-        log " ${GREEN}done${NC}${NL}"
+        log_err " ${GREEN}done${NC}${NL}"
     else
-        log " ${RED}failed${NC}${NL}"
+        log_err " ${RED}failed${NC}${NL}"
         exit 1
     fi
-    log "${WHITE}Gathered music genres: ${GREEN}${#MUSIC_GENRES[*]}${NC}"
+    log_err "${WHITE}Gathered music genres: ${GREEN}${#MUSIC_GENRES[*]}${NC}"
 }
 function gen_music_genres_menu() {
     # Generate menu with all existing music genres
-    log "${NL}${WHITE}Generating menu...${NC}${NL}"
+    log_err "${NL}${WHITE}Generating menu...${NC}${NL}"
     for mKey in "${!MUSIC_GENRES[@]}" ; do
-        log "${mKey}. ${MUSIC_GENRES[${mKey}]//_/' '}"
+        log_err "${mKey}. ${MUSIC_GENRES[${mKey}]//_/' '}"
     done
     echo >&2 ; read -rp "Select music genre: " SELECTED_GENRE
 
     [[ -z $SELECTED_GENRE ]] && die "You must select a music genre."
 
-    log "${NL}${WHITE}Selected: ${GREEN}${MUSIC_GENRES[${SELECTED_GENRE}]//_/' '}${NC}"
+    log_err "${NL}${WHITE}Selected: ${GREEN}${MUSIC_GENRES[${SELECTED_GENRE}]//_/' '}${NC}"
 }
 function gen_music_mix() {
     # Generate InstantMix based on selected genre
     echo -ne "${NL}${WHITE}Generating [${PURPLE}${MUSIC_GENRES[${SELECTED_GENRE}]//_/' '}${WHITE}] InstantMix...${NC}" >&2
     mapfile -t INSTANT_MIX < <(curl -sSL "${SERVER_ADDR}/MusicGenres/${MUSIC_GENRES[${SELECTED_GENRE}]//_/'%20'}/InstantMix?api_key=${API_KEY}" 2>/dev/null | jq -r '.Items[] | .Id')
     if [[ ${#INSTANT_MIX[*]} -gt 0 ]]; then
-        log " ${GREEN}${#INSTANT_MIX[*]}${WHITE} tracks.${NC}${NL}"
+        log_err " ${GREEN}${#INSTANT_MIX[*]}${WHITE} tracks.${NC}${NL}"
     else
-        log " ${RED}${#INSTANT_MIX[*]}${WHITE} tracks.${NC}${NL}"
+        log_err " ${RED}${#INSTANT_MIX[*]}${WHITE} tracks.${NC}${NL}"
         echo >&2 ; read -rp "Select another music genre? [Y,N]: " TRY_AGAIN
         if [[ -z $TRY_AGAIN || "${TRY_AGAIN,,}" == "n" ]]; then
             echo -e "${NL}${YELLOW}Leaving...${NC}${NL}" >&2
@@ -171,9 +134,11 @@ function gen_music_mix() {
     fi
 }
 function gen_playlist() {
+    local INDEX=0
+
     # Create dynamic playlist from generated InstantMix
     if [[ $DEBUG_MODE == true ]]; then
-        log "${PURPLE}[DEBUG]${WHITE} Creating dynamic playlist...${NC}${NL}"
+        log_err "${PURPLE}[DEBUG]${WHITE} Creating dynamic playlist...${NC}${NL}"
     fi
 
     # Required M3U extended header
@@ -190,6 +155,10 @@ function gen_playlist() {
 
     # Generate extended playlist data
     for SONG in "${INSTANT_MIX[@]}" ; do
+        ((++INDEX))
+
+        [[ $INDEX -gt $LIMIT ]] && continue
+
         # Gathering song data
         local SONG_DETAILS
         mapfile -t SONG_DETAILS < <(curl -sSL "${SERVER_ADDR}/Items/?api_key=${API_KEY}&userId=${USER_ID}&ids=${SONG}" 2>/dev/null | jq -rc '.Items[] | .AlbumArtist,.Name,.Album')
@@ -198,6 +167,8 @@ function gen_playlist() {
         # echo -e "#EXTINF:-1, ${SONG_DETAILS[0]} - ${SONG_DETAILS[1]}\n#EXTALB: ${SONG_DETAILS[2]}"
         echo -e "#EXTINF:0, ${SONG_DETAILS[0]} - ${SONG_DETAILS[1]}\n#EXTALB: ${SONG_DETAILS[2]}"
         echo "${SERVER_ADDR}/Audio/${SONG}/stream.mp3?api_key=${API_KEY}&audioBitRate=${AUDIO_BITRATE}&audioSampleRate=${SAMPLE_RATE}"
+
+        sleep 0.2
     done
 }
 function load_playlist() {
@@ -219,15 +190,24 @@ function load_playlist() {
                     --term-title='Playing song [${playlist-pos-1}/${playlist-count}]: ${metadata/by-key/Artist} - ${metadata/by-key/Album} - ${metadata/by-key/Title}' \
                     --playlist=- < <(gen_playlist)
             else
-                mpv --no-config \
-                    --shuffle \
-                    --no-audio-display \
-                    --term-osd-bar \
-                    --term-title='Playing song [${playlist-pos-1}/${playlist-count}]: ${metadata/by-key/Artist} - ${metadata/by-key/Album} - ${metadata/by-key/Title}' \
-                    --playlist=- < <(gen_playlist)
+                if [[ $(compare_version $MPV_MIN_VER $(get_mpv_version)) -eq 0 ]]; then
+                    mpv --no-config \
+                        --shuffle \
+                        --no-audio-display \
+                        --term-osd-bar \
+                        --term-title='Playing song [${playlist-pos-1}/${playlist-count}]: ${metadata/by-key/Artist} - ${metadata/by-key/Album} - ${metadata/by-key/Title}' \
+                        --playlist=- < <(gen_playlist)
+                else
+                    set_console_title "${SCRIPT_NAME}: ${MUSIC_GENRES[${SELECTED_GENRE}]//_/' '} - InstantMix"
+                    mpv --no-config \
+                        --shuffle \
+                        --no-audio-display \
+                        --term-osd-bar \
+                        --playlist=- < <(gen_playlist)
+                fi
             fi
         fi
-        exit $?
+        # exit $?
     elif [[ -n $BIN_MPG123 ]]; then
         set_console_title "${SCRIPT_NAME}: ${MUSIC_GENRES[${SELECTED_GENRE}]//_/' '} - Instant Mix"
         if [[ $DEBUG_MODE == true ]]; then
@@ -235,7 +215,7 @@ function load_playlist() {
         else
             mpg123 --rva-mix --long-tag --control --shuffle --buffer $BUFFER_SIZE --no-infoframe --smooth -v -@ - < <(gen_playlist)
         fi
-        exit $?
+        # exit $?
     else
         die "Unable to find proper audio backend."
     fi
@@ -244,15 +224,15 @@ function manage_player() {
     local OPERATION; OPERATION="$1"
     if [[ -n $BIN_PLAYCTL ]]; then
         playerctl -p mpv "$OPERATION" ; RET_CODE_CTL=$?
-        [[ ! $OPERATION == "status" ]] && playerctl -p mpv status
+        # [[ ! $OPERATION == "status" ]] && playerctl -p mpv status ; RET_CODE_CTL=$?
         exit $RET_CODE_CTL
     else
         die "You must have 'playerctl' installed to control music players."
-    fi  
+    fi
 }
 function run_player() {
     # Bootstrap everything
-    log "${NL}${WHITE}Initializing...${NC}${NL}"
+    log_err "${NL}${WHITE}Initializing...${NC}${NL}"
 
     # Server connection
     jellyfin_connect
@@ -274,14 +254,16 @@ function run_player() {
 }
 
 # Init
-load_xdg_defs
-load_config_file
+bootstrap
 
 # Header
 print_header
 
 # Usage
 [[ $1 == "-h" || $1 == "--help" ]] && print_usage
+if [[ $1 == "-l" || $1 == "--limit" ]]; then
+  shift ; LIMIT=$1
+fi
 
 # Arguments
 [[ $1 == "generate" ]] && CREATE_PLS_FILE=true
--- rmp.sh
+++ rmp.sh
@@ -15,91 +15,48 @@
 #  - Add 'mpg123' support
 #  - Add 'gst123' support
 #
-# Version: 0.0.3
+# Version: 0.0.4
 
 # Options
-set +o xtrace
-
-# Colors
-NC="\033[0m"
-NL="\n"
-BLUE="\033[1;34m"
-YELLOW="\033[1;33m"
-GREEN="\033[1;32m"
-RED="\033[1;31m"
-WHITE="\033[1;37m"
-PURPLE="\033[1;35m"
+[[ -r $HOME/.debug ]] && set -o xtrace || set +o xtrace
 
 # Config
 DEBUG_MODE=true
-CLEAR_SCREEN=true
+CLEAR_SCREEN=false
 SHOW_HEADER=true
 BUFFER_SIZE=0
 GST_VISUALIZATION_ENABLED=true
 GST_VISUALIZATION_TYPE="synaescope"
 MPV_GUI_ENABLED=false
+MPV_MIN_VER="0.34.0"
 
 # Internals
-BIN_GST123=$(which gst123 2>/dev/null)
-BIN_MPG123=$(which mpg123 2>/dev/null)
-BIN_MPV=$(which mpv 2>/dev/null)
-BIN_PLAYCTL=$(which playerctl 2>/dev/null)
-SCRIPT_DIR="$(dirname "$0")"
-SCRIPT_FILE="$(basename "$0")"
-SCRIPT_NAME="${SCRIPT_FILE//.sh/}"
-CONFIG_FILE="${SCRIPT_NAME}.conf"
+BIN_GST123=$(command -v gst123 2>/dev/null)
+BIN_MPG123=$(command -v mpg123 2>/dev/null)
+BIN_MPV=$(command -v mpv 2>/dev/null)
+BIN_VLC=$(command -v vlc 2>/dev/null)
+BIN_PLAYCTL=$(command -v playerctl 2>/dev/null)
+
+# Common
+if [[ -r "$(dirname "$0")/common/bash_funcs" ]]; then
+  source "$(dirname "$0")/common/bash_funcs" || ( echo -e "\nUnable to load '[script_dir]/common/bash_funcs' file.\n" && exit 255 )
+fi
 
 # Functions
-function die() {
-  echo -e "${NL}${RED}Error: ${YELLOW}${1}${NC}${NL}" >&2
-  exit 255
-}
-function log() {
-  echo -e "$1" >&2
-}
-function get_version() {
-  grep -i 'version' "$0" | awk '{ print $3 }' | head -n1
-}
-function set_console_title() {
-  echo -ne "\033]0;${1}\007"
-}
 function print_header() {
   [[ $CLEAR_SCREEN == true ]] && reset
-  [[ $SHOW_HEADER == true ]] && log "${NL}${BLUE}Simple Random CLI Music Player - ${GREEN}v$(get_version)${NC}${NL}"
+  [[ $SHOW_HEADER == true ]] && log_err "${NL}${BLUE}Simple Random CLI Music Player - ${GREEN}v$(get_version)${NC}${NL}"
 }
 function print_usage() {
   log "${NL}Usage: $SCRIPT_FILE [status|stop|play|pause|prev|next]${NL}"
   exit 1
 }
-function load_xdg_defs() {
-  if [[ -r ~/.config/user-dirs.dirs ]]; then
-    # shellcheck source=/dev/null
-    source ~/.config/user-dirs.dirs
-  fi
-  [[ -z $XDG_CONFIG_HOME ]] && XDG_CONFIG_HOME="$HOME/.config"
-}
-function load_config_file() {
-  if [[ -r "$XDG_CONFIG_HOME/$CONFIG_FILE" ]]; then
-    # shellcheck source=/dev/null
-    source "$XDG_CONFIG_HOME/$CONFIG_FILE"
-  elif [[ -r "$XDG_CONFIG_HOME/$SCRIPT_NAME/$CONFIG_FILE" ]]; then
-    # shellcheck source=/dev/null
-    source "$XDG_CONFIG_HOME/$SCRIPT_NAME/$CONFIG_FILE"
-  elif [[ -r "$SCRIPT_DIR/$CONFIG_FILE" ]]; then
-    # shellcheck source=/dev/null
-    source "$SCRIPT_DIR/$CONFIG_FILE"
-  fi
-}
-function get_mpv_mpris_lib() {
-  local MPV_MPRIS_PATH
-  if [[ -r "$XDG_CONFIG_HOME/mpv/scripts/mpris.so" ]]; then
-    MPV_MPRIS_PATH="$XDG_CONFIG_HOME/mpv/scripts/mpris.so"
-  elif [[ -r /etc/mpv/scripts/mpris.so ]]; then
-    MPV_MPRIS_PATH=/etc/mpv/scripts/mpris.so
-  else
-    MPV_MPRIS_PATH=
-  fi
-  echo -n "$MPV_MPRIS_PATH"
+function bootstrap() {
+  set_script_dir
+  set_script_file
+  set_config_file
+  load_xdg_defs
+  load_config_file
 }
 function set_music_dir() {
   # Get music dir from XDG Spec
@@ -119,17 +76,18 @@ function manage_player() {
   local OPERATION; OPERATION="$1"
   if [[ -n $BIN_PLAYCTL ]]; then
       playerctl -p mpv "$OPERATION" ; RET_CODE_CTL=$?
-      [[ ! $OPERATION == "status" ]] && playerctl -p mpv status
+      # [[ ! $OPERATION == "status" ]] && playerctl -p mpv status ; RET_CODE_CTL=$?
       exit $RET_CODE_CTL
   else
       die "You must have 'playerctl' installed to control music players."
-  fi  
+  fi
 }
 function run_player() {
   set_music_dir
 
   if [[ -n $MUSIC_DIR ]]; then
-    log "${NL}${WHITE}Initializing...${NC}${NL}"
+    log_err "${NL}${WHITE}Initializing...${NC}${NL}"
+    set_console_title "${SCRIPT_NAME}: Generating Random Mix..."
 
     # mpg123 sample command(s)
     # mpg123 --rva-mix --title --long-tag --control --shuffle -v "$MUSIC_DIR"/*/*/*.mp3
@@ -171,30 +129,40 @@ function run_player() {
               --term-title='Playing song [${playlist-pos-1}/${playlist-count}]: ${metadata/by-key/Artist} - ${metadata/by-key/Album} - ${metadata/by-key/Title}' \
               --playlist=- < <(gen_playlist)
         else
-          mpv --no-config \
-              --shuffle \
-              --no-audio-display \
-              --term-osd-bar \
-              --term-title='Playing song [${playlist-pos-1}/${playlist-count}]: ${metadata/by-key/Artist} - ${metadata/by-key/Album} - ${metadata/by-key/Title}' \
-              --playlist=- < <(gen_playlist)
+          if [[ $(compare_version $MPV_MIN_VER $(get_mpv_version)) -eq 0 ]]; then
+            mpv --no-config \
+                --shuffle \
+                --no-audio-display \
+                --term-osd-bar \
+                --term-title='Playing song [${playlist-pos-1}/${playlist-count}]: ${metadata/by-key/Artist} - ${metadata/by-key/Album} - ${metadata/by-key/Title}' \
+                --playlist=- < <(gen_playlist)
+          else
+            set_console_title "${SCRIPT_NAME}: Playing Random Mix..."
+            mpv --no-config \
+                --shuffle \
+                --no-audio-display \
+                --term-osd-bar \
+                --playlist=- < <(gen_playlist)
+          fi
         fi
       fi
-      exit $?
+      # exit $?
     elif [[ -n $BIN_MPG123 ]]; then
-      set_console_title "${SCRIPT_NAME}: Generating Random Mix..."
+      set_console_title "${SCRIPT_NAME}: Playing Random Mix..."
       if [[ $DEBUG_MODE == true ]]; then
         mpg123 --rva-mix --title --long-tag --control --shuffle --buffer $BUFFER_SIZE --smooth -vvv -@ - < <(gen_playlist)
       else
         mpg123 --rva-mix --title --long-tag --control --shuffle --buffer $BUFFER_SIZE --smooth -v -@ - < <(gen_playlist)
       fi
-      exit $?
+      # exit $?
     elif [[ -n $BIN_GST123 ]]; then
+      set_console_title "${SCRIPT_NAME}: Playing Random Mix..."
       if [[ $GST_VISUALIZATION_ENABLED == true ]]; then
         gst123 -v "$GST_VISUALIZATION_TYPE" --name="$SCRIPT_NAME" --shuffle -@ - < <(gen_playlist) 2>&1 | grep -vi themes
       else
         gst123 -x --name="$SCRIPT_NAME" --shuffle -@ - < <(gen_playlist) 2>&1 | grep -vi themes
       fi
-      exit $?
+      # exit $?
     else
       die "Unable to find proper audio backend."
     fi
@@ -204,8 +172,7 @@ function run_player() {
 }
 
 # Init
-load_xdg_defs
-load_config_file
+bootstrap
 
 # Header
 print_header
